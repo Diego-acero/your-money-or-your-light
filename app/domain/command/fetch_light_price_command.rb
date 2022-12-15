d46b8b2cbf8040d8c44915e5001d71f4d4246ace
Diego Acero <diegoaceroarguelles@gmail.com>
2022-12-15 11:31:21 +0100

    Added light price fetch command

---
+++ app/domain/command/fetch_light_price_command.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class FetchLightPriceCommand
+
+  def self.call
+    new.call
+  end
+
+  def call
+    #Call external service for getting prices
+    #
+  end
+
+end
